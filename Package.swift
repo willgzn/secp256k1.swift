0923566ba8b6b65aeb48eb0282d2c6d7179be3e1
William Guezennec <william.guezennec@gmail.com>
2024-06-13 19:11:18 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "secp256k1",
+    name: "secp256k1bis",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
